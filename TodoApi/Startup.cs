566aa364444a9a97a55a33efbe916e30a38e5640
unknown <e.asterion@gmail.com>
2018-05-11 16:36:36 -0400

    Programa para ASP NET

---
+++ TodoApi/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
+            services.AddMvc();
+        }        
+        public void Configure( IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
